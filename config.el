9855b30ade72f8178bf2a6f527a6ff77e5916783
Jacob Moena <jacmoe.dk@gmail.com>
2022-04-03 17:55:46 +0200

    Roam templates - I really have little idea of what I am doing

--- config.el
+++ config.el
@@ -215,67 +215,21 @@
 (after! org-roam
   (setq org-roam-capture-templates
         `(("n" "note" plain
-           ,(format "#+title: ${title}\n%%[%s/template/note.org]" org-roam-directory)
+           ,(format "#+title: ${title}\n%%" org-roam-directory)
            :target (file "note/%<%Y%m%d%H%M%S>-${slug}.org")
            :unnarrowed t)
           ("r" "thought" plain
-           ,(format "#+title: ${title}\n%%[%s/template/thought.org]" org-roam-directory)
+           ,(format "#+title: ${title}\n%%" org-roam-directory)
            :target (file "thought/%<%Y%m%d%H%M%S>-${slug}.org")
            :unnarrowed t)
           ("t" "topic" plain
-           ,(format "#+title: ${title}\n%%[%s/template/topic.org]" org-roam-directory)
+           ,(format "#+title: ${title}\n%%" org-roam-directory)
            :target (file "topic/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t)
-          ("c" "contact" plain
-           ,(format "#+title: ${title}\n%%[%s/template/contact.org]" org-roam-directory)
-           :target (file "contact/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t)
-          ("p" "project" plain
-           ,(format "#+title: ${title}\n%%[%s/template/project.org]" org-roam-directory)
-           :target (file "project/%<%Y%m%d>-${slug}.org")
-           :unnarrowed t)
-          ("i" "invoice" plain
-           ,(format "#+title: %%<%%Y%%m%%d>-${title}\n%%[%s/template/invoice.org]" org-roam-directory)
-           :target (file "invoice/%<%Y%m%d>-${slug}.org")
-           :unnarrowed t)
-          ("f" "ref" plain
-           ,(format "#+title: ${title}\n%%[%s/template/ref.org]" org-roam-directory)
-           :target (file "ref/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t)
-          ("w" "works" plain
-           ,(format "#+title: ${title}\n%%[%s/template/works.org]" org-roam-directory)
-           :target (file "works/%<%Y%m%d%H%M%S>-${slug}.org")
-           :unnarrowed t)
-          ("s" "secret" plain "#+title: ${title}\n\n"
-           :target (file "secret/%<%Y%m%d%H%M%S>-${slug}.org.gpg")
            :unnarrowed t))
         ;; Use human readable dates for dailies titles
         org-roam-dailies-capture-templates
         '(("d" "default" entry "* %?"
            :target (file+head "%<%Y-%m-%d>.org" "#+title: %<%B %d, %Y>\n\n")))))
-
-(after! org-roam
-  ;; Offer completion for #tags and @areas separately from notes.
-  (add-to-list 'org-roam-completion-functions #'org-roam-complete-tag-at-point)
-
-  ;; Automatically update the slug in the filename when #+title: has changed.
-  (add-hook 'org-roam-find-file-hook #'org-roam-update-slug-on-save-h)
-
-  ;; Make the backlinks buffer easier to peruse by folding leaves by default.
-  (add-hook 'org-roam-buffer-postrender-functions #'magit-section-show-level-2)
-
-  ;; List dailies and zettels separately in the backlinks buffer.
-  (advice-add #'org-roam-backlinks-section :override #'org-roam-grouped-backlinks-section)
-
-  ;; Open in focused buffer, despite popups
-  (advice-add #'org-roam-node-visit :around #'+popup-save-a)
-
-  ;; Make sure tags in vertico are sorted by insertion order, instead of
-  ;; arbitrarily (due to the use of group_concat in the underlying SQL query).
-  (advice-add #'org-roam-node-list :filter-return #'org-roam-restore-insertion-order-for-tags-a)
-
-  ;; Add ID, Type, Tags, and Aliases to top of backlinks buffer.
-  (advice-add #'org-roam-buffer-set-header-line-format :after #'org-roam-add-preamble-a))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;                                                                                  ;;
