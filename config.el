aa1d5462efa5d16fe86998708fbde5c6511f2825
jacmoe <jacmoe.dk@gmail.com>
2023-02-24 16:04:38 +0100

    Boon : updated layout to be more VIM-friendly

--- config.el
+++ config.el
@@ -21,17 +21,17 @@
 (defvar my-keyboard-variant "qwerty")                 ; colemak or qwerty
 
 ;; (defvar my-dark-theme 'ef-night)
-;; (defvar my-dark-theme 'ef-cherie)
+(defvar my-dark-theme 'ef-cherie)
 ;; (defvar my-dark-theme 'ef-winter)
 ;; (defvar my-dark-theme 'ef-autumn)
 ;; (defvar my-dark-theme 'ef-duo-dark)
-(defvar my-dark-theme 'ef-tritanopia-dark)
+;; (defvar my-dark-theme 'ef-tritanopia-dark)
 
 ;; (defvar my-light-theme 'ef-day)
 (defvar my-light-theme 'ef-tritanopia-light)
 
 (defvar my-main-theme my-dark-theme)
-(defvar my-theme-shade "dark")                        ; can be light or dark. Used to color the Boon-mode cursor
+(defvar my-theme-shade "dark")                        ; can be light or dark.
 
 (defvar my-monospace-font "Overpass Mono")            ; Font to use for code
 (defvar my-variablespace-font "Overpass")             ; Font to use for writing
@@ -692,12 +692,28 @@
    (if (equal my-keyboard-variant "colemak")
        ;; we are using Colemak
        (progn
-         (define-key boon-moves-map "ø" 'move-end-of-line)
+        (define-key boon-moves-map "ø" 'move-end-of-line)
          (require 'boon-colemak)
+
          (define-key boon-moves-map "l" 'move-beginning-of-line)
+         (define-key boon-moves-map "u" 'backward-sentence)
+         (define-key boon-moves-map "U" 'ignore)
+         (define-key boon-moves-map "y" 'forward-sentence)
+         (define-key boon-moves-map "Y" 'ignore)
          (define-key boon-moves-map ";" 'move-end-of-line)
-         (define-key boon-moves-map "E" 'backward-sentence)
-         (define-key boon-moves-map "I" 'forward-sentence))
+
+         (define-key boon-moves-map "H"  '("⇠" . boon-smarter-backward))        ;word
+         (define-key boon-moves-map "h"  '("←" . backward-char))                ;character
+         (define-key boon-moves-map "N"  'forward-paragraph)                    ;faster down
+         (define-key boon-moves-map "n"  '("↓" . next-line))                    ;down
+         (define-key boon-moves-map "e"  '("↑" . previous-line))                ;up
+         (define-key boon-moves-map "E"  'backward-paragraph)                   ;faster up
+         (define-key boon-moves-map "i"  '("→" . forward-char))                 ;character
+         (define-key boon-moves-map "I"  '("→" . boon-smarter-forward))         ;word
+
+         (define-key boon-moves-map "o"  '("hop" . avy-goto-word-1))
+         (define-key boon-moves-map "O"  'avy-goto-char))
+
      ;; we are using Qwerty
      (progn
        (define-key boon-moves-map "æ" 'boon-smarter-forward)
